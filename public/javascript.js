9e366ed540b9b52a4b93d3343d4234c00fa0ba88
Un Sang Park <iloveunhs@naver.com>
2018-10-27 02:20:43 +0900

    Add files via upload

---
+++ public/javascript.js
@@ -0,0 +1,54 @@
+$(document).ready(function()
+{
+  $('#submit').click(function()
+  {
+    var c = confirm("Do you want to submit it?");
+    if(c==true)
+    {
+      alert("submission is complete.");
+    }
+    else
+    {
+      alert("submission has been canceled.");
+    }
+  })
+  $('#reset').click(function(){
+    alert("Do you want to reset it?");
+  })
+});
+
+///////////////////////////////////////////////////////////////
+
+function bigger(x)
+{
+  x.style.width="40%";
+  x.style.height="40%";
+}
+function normal(x)
+{
+  x.style.width="30%";
+  x.style.height="30%";
+}
+
+function bigger1(x)
+{
+  x.style.width="20%";
+  x.style.height="20%";
+}
+function normal1(x)
+{
+  x.style.width="15%";
+  x.style.height="15%";
+}
+
+
+/////////////////////////////////////////////////////////////////
+
+$("#animate-demo").animate({left:"500px",opacity:"0.5"}, 2000);
+
+setTimeout(function(){ $('#sentence1').removeAttr('hidden');},2500);
+setTimeout(function(){ $('#sentence2').removeAttr('hidden');},3500);
+setTimeout(function(){ $('#sentence3').removeAttr('hidden');},4500);
+setTimeout(function(){ $('#sentence4').removeAttr('hidden');},5500);
+setTimeout(function(){ $('#sentence5').removeAttr('hidden');},6500);
+setTimeout(function(){ $('#sentence6').removeAttr('hidden');},7500);
