7746defc3c2d66daf5e357cf3e98160a9db22045
ghyeon0 <xcm1321@gmail.com>
2018-10-26 22:06:50 +0900

    Basic Function Added

--- server.js
+++ server.js
@@ -8,3 +8,27 @@ var server = app.listen(9000, function(){
 app.get("/", function(req, res){
     res.send("Hello World!");
 });
+
+app.get("/battery", function(req, res){
+    res.send("Battery Checked");
+});
+
+app.get("/glue", function(req, res){
+    res.send("Glue Checked");
+});
+
+app.get("/charger", function(req, res){
+    res.send("Charger Checked");
+});
+
+app.get("/tape", function(req, res){
+    res.send("Tape Checked");
+});
+
+app.get("/punch", function(req, res){
+    res.send("Punch Checked");
+});
+
+app.get("/scissors", function(req, res){
+    res.send("Scissors Checked");
+});
